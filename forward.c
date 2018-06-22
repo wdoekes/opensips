0ed7537f4e148c377b68540535c275b0cecf4120
Walter Doekes <walter+github@wjd.nu>
2018-06-22 13:13:12 +0000

    GN: Backport reverse_hex2int fixes against ignored transactions
    See: https://github.com/OpenSIPS/opensips/issues/1407
    
        commit 095dc57300b9665b090e999a540db8b391f8d22e
        Author: Bogdan-Andrei Iancu <bogdan@opensips.org>
        Date:   Tue Jan 30 14:07:42 2018 +0200
    
            Fix reverse_hex2int() prototype
    
    	As the computed value is an unsigned (as data size), it cannot
            be returned as int as it will overflow and get converted to a
            negative value. This will colide with the negative error ret
            code -1
    
            Reported by Daniel Zanutti.
    
        Author: Bogdan-Andrei Iancu <bogdan@opensips.org>
        Date:   Tue Jan 30 14:42:22 2018 +0200
    
            Explicit reset of conn ID on error
    
            if reverse_hex2int fails, be sure the conn id stays 0.
    
    This should fix incidental ignored responses to packets generated by
    us. They all had this in the Via branch:
    
        branch=z9hG4bKbc96.1c300008.0
                                  ^--- little endian 8, thus above 2^31

--- forward.c
+++ forward.c
@@ -576,7 +576,7 @@ int forward_reply(struct sip_msg* msg)
 	unsigned int new_len;
 	struct sr_module *mod;
 	int proto;
-	int id; /* used only by tcp*/
+	unsigned int id; /* used only by tcp*/
 	struct socket_info *send_sock;
 #ifdef USE_TCP
 	char* s;
@@ -638,7 +638,8 @@ int forward_reply(struct sip_msg* msg)
 		if (msg->via1->i&&msg->via1->i->value.s){
 			s=msg->via1->i->value.s;
 			len=msg->via1->i->value.len;
-			id=reverse_hex2int(s, len);
+			if (reverse_hex2int(s, len, &id)<0)
+				id = 0;
 		}
 	}
 #endif
@@ -651,7 +652,7 @@ int forward_reply(struct sip_msg* msg)
 		goto error;
 	}
 
-	if (msg_send(send_sock, proto, to, id, new_buf, new_len)<0) {
+	if (msg_send(send_sock, proto, to, (int)id, new_buf, new_len)<0) {
 		update_stat( drp_rpls, 1);
 		goto error0;
 	}
