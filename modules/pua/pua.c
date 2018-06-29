17a47113b2583d60cf6becd8b0b316c624bfebeb
Walter Doekes <walter+github@wjd.nu>
2018-06-29 15:36:52 +0000

    GN: Don't purge opensips_pua table on startup
    On MySQL-replicated databases this causes deletions that we may not want.

--- modules/pua/pua.c
+++ modules/pua/pua.c
@@ -574,7 +574,9 @@ static int db_restore(void)
 	pua_dbf.free_result(pua_db, res);
 	res = NULL;
 
-	if(pua_dbf.delete(pua_db, 0, 0 , 0, 0) < 0)
+	/* DISABLED: we don't want to delete anything here if we're
+	 * using a replicating DB */
+	if(0 && pua_dbf.delete(pua_db, 0, 0 , 0, 0) < 0)
 	{
 		LM_ERR("while deleting information from db\n");
 		goto error;
